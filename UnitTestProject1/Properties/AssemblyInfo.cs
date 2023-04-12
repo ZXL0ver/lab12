401ca318999cc8386ca0519b32da4624343e7828
ZXL0ver <130565880+ZXL0ver@users.noreply.github.com>
2023-04-12 21:23:28 +0300

    Add files via upload

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ed782429-f8cd-457b-9309-ddf7c0431299")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PaymentServiceUnitTest
+{
+    [TestClass]
+    public class PaymentServiceTest
+    {
+        [TestMethod]
+        public void TestBankTransferService()
+        {
+            PaymentService bankTransferService = new BankTransferService();
+            PaymentService creditCardService = new CreditCardService();
+            PaymentService payPalService = new PayPalService();
+
+            bankTransferService.SetNext(creditCardService).SetNext(payPalService);
+
+            Assert.IsFalse(bankTransferService.Handle(2500));
+            Assert.IsTrue(bankTransferService.Handle(1000));
+            Assert.IsTrue(bankTransferService.Handle(500));
+        }
+
+        [TestMethod]
+        public void TestCreditCardService()
+        {
+            PaymentService creditCardService = new CreditCardService();
+            PaymentService payPalService = new PayPalService();
+
+            creditCardService.SetNext(payPalService);
+
+            Assert.IsFalse(creditCardService.Handle(2500));
+            Assert.IsTrue(creditCardService.Handle(0));
+            Assert.IsTrue(creditCardService.Handle(500));
+        }
+
+        [TestMethod]
+        public void TestPayPalService()
+        {
+            PaymentService payPalService = new PayPalService();
+
+            Assert.IsFalse(payPalService.Handle(2500));
+            Assert.IsTrue(payPalService.Handle(2000));
+            Assert.IsTrue(payPalService.Handle(500));
+        }
+    }
+}
+
